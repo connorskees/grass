26ae7f38b45925e5c99798990382453ed77c9b36
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-01-06 18:26:07 -0500

    Implement basic error handling

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,38 @@
+use crate::common::Pos;
+use std::error::Error;
+use std::fmt::{self, Display};
+use std::io;
 
+#[derive(Debug)]
+pub struct SassError {
+    message: String,
+    pos: Pos
+}
+
+impl SassError {
+    pub fn new<S: Into<String>>(message: S, pos: Pos) -> Self {
+        SassError { message: message.into(), pos }
+    }
+
+    pub fn unexpected_eof(pos: Pos) -> Self {
+        SassError { message: String::from("unexpected eof"), pos }
+    }
+}
+
+impl Display for SassError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "error: {} at {}", self.message, self.pos)
+    }
+}
+
+impl From<io::Error> for SassError {
+    fn from(error: io::Error) -> Self {
+        SassError{ pos: Pos::new(), message: format!("{}", error) }
+    }
+}
+
+impl Error for SassError {
+    fn description(&self) -> &'static str {
+        "SASS parsing error"
+    }
+}
