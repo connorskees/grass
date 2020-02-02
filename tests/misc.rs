231252a9d97e7875c59c6805d93105be69361e04
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-02-02 17:44:39 -0500

    Remove redundant tests

--- tests/misc.rs
+++ tests/misc.rs
@@ -3,12 +3,6 @@
 #[macro_use]
 mod macros;
 
-// test!(
-//     combines_hyphens,
-//     "a {\n  foo: bar - baz;\n}\n",
-//     "a {\n  foo: bar-baz;\n}\n"
-// );
-test!(does_not_combine_hyphens, "a {\n  foo: bar -baz;\n}\n");
 test!(
     ident_starts_with_hyphen,
     "a {\n  foo: -webkit-bar-baz;\n}\n"
@@ -25,6 +19,6 @@ test!(
     "a {\n  height: !important 1;\n}\n"
 );
 test!(
-    double_newline_between_unrelated_styles,
+    emits_double_newline_between_unrelated_styles,
     "a {\n  color: red;\n}\n\nb {\n  color: blue;\n}\n"
 );
