042c5f4f2ffdaeec79bc7e25cfd6b73dd56a9ee5
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-02-16 10:27:51 -0500

    Null should not be preserved

--- tests/values.rs
+++ tests/values.rs
@@ -10,7 +10,7 @@ test!(space_list_number, "a {\n  color: 1 2 3;\n}\n");
 test!(comma_space_list_number, "a {\n  color: 1 1, 2 2, 3 3;\n}\n");
 test!(preserves_keyword_true, "a {\n  color: true;\n}\n");
 test!(preserves_keyword_false, "a {\n  color: false;\n}\n");
-test!(preserves_keyword_null, "a {\n  color: null;\n}\n");
+test!(does_not_preserve_keyword_null, "a {\n  color: null;\n}\n", "");
 test!(preserves_keyword_auto, "a {\n  color: auto;\n}\n");
 test!(preserves_keyword_initial, "a {\n  color: initial;\n}\n");
 test!(preserves_keyword_infinity, "a {\n  color: infinity;\n}\n");
