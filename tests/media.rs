260b357bba132097e2f17a96f354ae94d2c40e6d
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-02-22 17:08:58 -0500

    Test for unknown at rules

--- tests/media.rs
+++ tests/media.rs
@@ -12,3 +12,7 @@ test!(
     "a {\n  @media foo {\n  color: red;\n  }\n}\n",
     "@media foo {\n  a {\n    color: red;\n  }\n}\n"
 );
+test!(
+    basic_unknown_at_rule,
+    "@foo {\n  a {\n    color: red;\n  }\n}\n"
+);
