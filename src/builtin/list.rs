ef480ad1f854f0c846339725a1f04f8abd3bc1c7
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-03-20 10:03:54 -0400

    length() max args check

--- src/builtin/list.rs
+++ src/builtin/list.rs
@@ -8,6 +8,7 @@ pub(crate) fn register(f: &mut HashMap<String, Builtin>) {
     f.insert(
         "length".to_owned(),
         Box::new(|args, _| {
+            max_args!(args, 1);
             let len = match arg!(args, 0, "list") {
                 Value::List(v, _) => Number::from(v.len()),
                 _ => Number::from(1),
