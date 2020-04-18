47b7ece6c57386c14cffd13cafaca9b26a00de52
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-04-18 19:08:35 -0400

    update tests to include new API changes

--- tests/macros.rs
+++ tests/macros.rs
@@ -8,10 +8,10 @@ macro_rules! test {
         #[allow(non_snake_case)]
         fn $func() {
             let mut buf = Vec::new();
-            grass::StyleSheet::new($input.to_string())
-                .expect(concat!("failed to parse on ", $input))
-                .print_as_css(&mut buf)
-                .expect(concat!("failed to pretty print on ", $input));
+            grass::StyleSheet::new($input.to_string(), &mut buf)
+                .expect(concat!("failed to parse on ", $input));
+                // .print_as_css()
+                // .expect(concat!("failed to pretty print on ", $input));
             assert_eq!(
                 String::from($input),
                 String::from_utf8(buf).expect("produced invalid utf8")
@@ -24,10 +24,8 @@ macro_rules! test {
         #[allow(non_snake_case)]
         fn $func() {
             let mut buf = Vec::new();
-            grass::StyleSheet::new($input.to_string())
-                .expect(concat!("failed to parse on ", $input))
-                .print_as_css(&mut buf)
-                .expect(concat!("failed to pretty print on ", $input));
+            grass::StyleSheet::new($input.to_string(), &mut buf)
+                .expect(concat!("failed to parse on ", $input));
             assert_eq!(
                 String::from($output),
                 String::from_utf8(buf).expect("produced invalid utf8")
@@ -45,7 +43,8 @@ macro_rules! error {
         #[test]
         #[allow(non_snake_case)]
         fn $func() {
-            match grass::StyleSheet::new($input.to_string()) {
+            let mut buf = Vec::new();
+            match grass::StyleSheet::new($input.to_string(), &mut buf) {
                 Ok(..) => panic!("did not fail"),
                 Err(e) => assert_eq!($err, e.to_string()
                                                 .chars()
