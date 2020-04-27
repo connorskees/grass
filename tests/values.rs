ce32a7e5ac0a6b19e0578da1f46bdcfc1040dc0e
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-04-26 23:21:18 -0400

    explicitly test for equality

--- tests/values.rs
+++ tests/values.rs
@@ -71,31 +71,6 @@ test!(
     "a {\n  color: \"f\" foo;\n}\n"
 );
 test!(
-    color_equals_color,
-    "a {\n  color: red == red;\n}\n",
-    "a {\n  color: true;\n}\n"
-);
-test!(
-    color_does_not_equal_color,
-    "a {\n  color: red != red;\n}\n",
-    "a {\n  color: false;\n}\n"
-);
-test!(
-    unquoted_ident_eq_unquoted_ident,
-    "a {\n  color: foo == foo;\n}\n",
-    "a {\n  color: true;\n}\n"
-);
-test!(
-    dblquoted_ident_eq_unquoted_ident,
-    "a {\n  color: \"foo\" == foo;\n}\n",
-    "a {\n  color: true;\n}\n"
-);
-test!(
-    dblquoted_ident_eq_sglquoted_ident,
-    "a {\n  color: \"foo\" == 'foo';\n}\n",
-    "a {\n  color: true;\n}\n"
-);
-test!(
     many_parens,
     "a {\n  color: (((((red)))));\n}\n",
     "a {\n  color: red;\n}\n"
