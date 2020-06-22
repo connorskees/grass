c031d42f41c6fb9502a160747fe211d3cec62bfa
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2020-06-22 10:31:01 -0400

    change how filenames beginning with _ are tested for

--- src/parse/import.rs
+++ src/parse/import.rs
@@ -56,7 +56,7 @@ impl<'a> Parser<'a> {
 
         let paths = [
             path_buf.with_file_name(name).with_extension("scss"),
-            path_buf.with_file_name(format!("_{}.scss", name.to_str().unwrap())),
+            path_buf.with_file_name(format!("_{}", name.to_str().unwrap())).with_extension("scss"),
             path_buf.clone(),
             path_buf.join("index.scss"),
             path_buf.join("_index.scss"),
