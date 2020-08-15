ba7c8527976121d6ab4a4c9c6e61e0a26b86a9ba
Midas Lambrichts <midaslamb@gmail.com>
2020-08-15 10:32:47 +0200

    Resolved some lints

--- fuzz/fuzz_targets/from_string_parsing.rs
+++ fuzz/fuzz_targets/from_string_parsing.rs
@@ -1,13 +1,12 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use grass::{from_path, from_string, Options};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let options = Options::default();
+        let options = grass::Options::default();
 
-        grass::from_string(
+        let _ = grass::from_string(
             s.to_owned(),
             &options
         );
