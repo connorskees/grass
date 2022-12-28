494acb99338de25796ec8591bc7ddea6fb867c3e
Connor Skees <connor1skees@gmail.com>
2022-12-28 15:58:49 -0500

    update error message in test

--- tests/media.rs
+++ tests/media.rs
@@ -572,5 +572,5 @@ error!(
 );
 error!(
     empty_query_after_resolving_interpolation,
-    "@media #{null} {}", "Error: expected no more input."
+    "@media #{null} {}", "Error: Expected identifier."
 );
